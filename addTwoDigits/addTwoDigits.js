cda9d7c15cc7bafbcf9b49fe45996844e440596b
Dennis <dvanwetering@gmail.com>
2018-05-02 15:33:44 +0200

    Added the twoDigits function.

---
+++ addTwoDigits/addTwoDigits.js
@@ -0,0 +1,5 @@
+function addTwoDigits(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoDigits(5, 6));
---
+++ addTwoDigits/readme.md
@@ -0,0 +1,3 @@
+# Objective
+
+Write a function that takes two numbers and add the numbers togheter.
