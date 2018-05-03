dffe681bf8df8ddf244d19ef6169496b37addc72
Dennis <dvanwetering@gmail.com>
2018-05-03 14:15:11 +0200

    Added the isPalindrome function.

---
+++ palinDrome/palinDrome.js
@@ -0,0 +1,5 @@
+// console.log(isPalindrome('A man, a plan, a canal. Panama'));
+// console.log(isPalindrome('My age is 0, 0 sii ega ym.'));
+// console.log(isPalindrome('_codegod'));
+// console.log(isPalindrome('legendary'));
+// console.log(isPalindrome('.0_0 (: /-\ :) 0-0'));
---
+++ palinDrome/readme.md
@@ -0,0 +1,3 @@
+# Objective
+
+Write a function that takes in a string that ignores special caracters and underscores, is case sensitive, ignores spaces and sees if it is a palindrome.
